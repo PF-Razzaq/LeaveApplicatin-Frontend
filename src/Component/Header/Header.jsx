7cd99c3cf0ecb5708f616bf865c6eeddcfa9561d
Abdul Razzaq <abdulrazzaqchohan1@gmail.com>
2024-02-22 17:22:09 +0500

    first commit

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <Link>LMS</Link>I love Pakistan
+      </div>
+    </>
+  );
+};
+
+export default Header;
